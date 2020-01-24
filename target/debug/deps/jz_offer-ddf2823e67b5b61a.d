d43135353fb67823e5401a46571f11b91d1624b9
tryfor_ <619896855@qq.com>
2020-01-24 13:32:50 +0800

    add dependency lazy_static = 1.2.0

---
+++ target/debug/deps/jz_offer-ddf2823e67b5b61a.d
@@ -0,0 +1,6 @@
+/Users/xiaohongwei/projects/for_offer/target/debug/deps/jz_offer-ddf2823e67b5b61a: src/main.rs src/two_stack_007.rs
+
+/Users/xiaohongwei/projects/for_offer/target/debug/deps/jz_offer-ddf2823e67b5b61a.d: src/main.rs src/two_stack_007.rs
+
+src/main.rs:
+src/two_stack_007.rs:
